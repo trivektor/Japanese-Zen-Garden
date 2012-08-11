47c1c70418fea4d7fdc1a523860736461e9514ae
trivektor <trivektor@gmail.com>
2012-08-11 00:16:02 -0700

    Initial Commit

---
+++ Japanese Zen Garden 2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Japanese Zen Garden 2
+//
+//  Created by Tri Vuong on 8/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
